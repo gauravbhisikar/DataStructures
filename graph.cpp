efaea6e2c3d4df151815037fd7b499b7c2c25f29
Gaurav bhisikar <gaurav.bhisikar@gmail.com>
2020-10-21 00:15:36 +0530

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,40 +1,94 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+template<typename dataType>
 class Graph
 {	
-	int V;
-	list<int> *l;
+	map<dataType,list<dataType> > adjList;
+	
 	public:
-		Graph(int V)
-		{
-			this->V = V;
-			l = new list<int>[V]; 
-		}
-
+		
 		void addEdge(int x,int y)
 		{
-			l[x].push_back(y);
-			l[y].push_back(x);
+			adjList[x].push_back(y);
+			adjList[y].push_back(x);
+		}
+
+		
+		void bfs(dataType srcNode)
+		{
+			map<dataType,int> visited;
+			queue<dataType> q;
+			q.push(srcNode);
+			visited[srcNode] = true;
+
+			while(!q.empty())
+			{
+				dataType node = q.front();
+				cout<<node<<" ";
+				q.pop();
+
+				for(int nbr:adjList[node])
+				{
+					if(!visited[nbr])
+					{
+						q.push(nbr);
+						visited[nbr] = true;
+					}
+				}
+			}
+			cout<<endl;
+
+		}
+
+		void dfsHelper(dataType src, map<dataType,bool> &visited)
+		{
+			cout<< src <<" ";
+			visited[src] = true;
+			for(dataType nbr: adjList[src])
+			{
+				if(!visited[nbr])
+				{
+					dfsHelper(nbr,visited);
+				}
+			}
+		}
+
+		void dfs(dataType src)
+		{
+			map<dataType,bool> visited;
+			for (auto p:adjList)
+			{
+				dataType node = p.first;
+				visited[node] = false;
+			}
+			dfsHelper(src,visited);
+			cout<<endl;
 		}
 
 		void printAdjList()
 		{
-			for(int i=0;i<V;i++){
-				cout<<"Vertex "<<i<<"->";
-				for(int n:l[i]){
-					cout<<n<<",";
-				}
-				cout<<endl;
+			
+		for(auto a:adjList)
+		{
+			cout<<a.first<<"->";
+			for(auto it = a.second.begin(); it != a.second.end(); ++it)
+			{
+				cout<<*it<<',';
+				
 			}
+			cout<<endl;
+
 		}
+		}
+
 
 };
 
 
 int main()
 {
-	Graph g(6);
+	Graph<int> g;
 	g.addEdge(0,1);
 	g.addEdge(0,2);
 	g.addEdge(1,2);
@@ -45,5 +99,7 @@ int main()
 	g.addEdge(4,5);
 	g.addEdge(5,3);
 	g.printAdjList();
+	g.bfs(3);
+	g.dfs(3);
 
 }
